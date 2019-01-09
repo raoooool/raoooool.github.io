cea3631e0c2315ac948d58ffe19ada102475600c
Raool <838326280@qq.com>
2019-01-09 21:11:43 +0800

    【增加】上线贪吃蛇小游戏

---
+++ snake/index.js
@@ -0,0 +1,187 @@
+/**
+ * @param Snake 蛇类
+ */
+function Snake() {
+    //constructor
+    this.body = [];
+    for (let i = 300; i < 310; i++) {
+        this.body.push(map[i]);
+    }
+    this.head = this.body[this.body.length - 1];
+
+    //方法
+    //行走方法
+    this.walk = function (direction) {
+        util.clearAllInterval();
+        setInterval(() => {
+            switch (direction) {
+                case 1:
+                    this.head = map[this.head.INDEX - 32]; //上
+                    break;
+                case 2:
+                    this.head = map[this.head.INDEX + 1]; //右
+                    break;
+                case 3:
+                    this.head = map[this.head.INDEX + 32]; //下
+                    break;
+                case 4:
+                    this.head = map[this.head.INDEX - 1]; //左
+                    break;
+            }
+            this.body.push(this.head);
+            //判断吃到食物
+            if (this.head.INDEX == food.INDEX) {
+                food = new Food;
+                score.addScore();
+            } else {
+                this.body.shift();
+            }
+            util.freshColor();
+            util.judgeGameOver();
+        }, 100);
+    }
+}
+
+/**
+ * @param MapPoint 地图点类
+ */
+let INDEX = -1;
+function MapPoint() {
+    //constructor
+    INDEX++;
+    let mapPoint = document.createElement('div');
+    mapPoint.style = 'background-color:whitesmoke;margin:5px;width:15px;height:15px;border-radius:15px;';
+    document.getElementById('map').appendChild(mapPoint);
+    mapPoint.state = 0;
+    mapPoint.INDEX = INDEX;
+    //方法
+    //设置点状态
+    mapPoint.setState = function (s) {
+        this.state = s;
+        switch (this.state) {
+            case 1:
+                this.style.backgroundColor = 'black';
+                break;
+            default:
+                this.style.backgroundColor = 'whitesmoke';
+                break;
+        }
+    }
+
+    return mapPoint;
+}
+
+/**
+ * @param Map 地图类
+ */
+function Map() {
+    //constructor
+    let map = [];
+    for (let i = 0; i < 1024; i++) {
+        map.push(new MapPoint);
+    }
+    return map;
+}
+
+/**
+ * @param Food 食物类
+ */
+function Food() {
+    function GenerateFood() {
+        let r = Math.floor(Math.random() * 1000);
+        if (r % 32 == 0 || r % 32 == 31 || (r > 0 && r < 32) || (r > 992 && r < 1024)) r = GenerateFood();
+        for (let i = 0; i < snake.body.length; i++) {
+            if (r == snake.body[i].INDEX) r = GenerateFood();
+        }
+        return r;
+    }
+    return map[GenerateFood()];
+}
+
+/**
+ * @param Util 工具类
+ */
+function Util() {
+    //刷新颜色
+    this.freshColor = function () {
+        for (let i = 0; i < map.length; i++) {
+            map[i].style.backgroundColor = 'whitesmoke';
+            if(map[i].INDEX % 32 == 0 || map[i].INDEX % 32 == 31 || (map[i].INDEX > 0 && map[i].INDEX < 32) || (map[i].INDEX > 992 && map[i].INDEX < 1024)){
+                map[i].style.backgroundColor = 'gainsboro';
+            }
+        }
+        for (let i = 0; i < snake.body.length; i++) {
+            snake.body[i].style.backgroundColor = 'black';
+        }
+        if (food) food.style.backgroundColor = 'green';
+        snake.head.style.backgroundColor = 'red';
+    }
+    //判断GameOver
+    this.judgeGameOver = function () {
+        let head = snake.head;
+        let body = snake.body;
+        let isGameOver = false;
+        //判断撞墙
+        if (head.INDEX % 32 == 0 || head.INDEX % 32 == 31 || (head.INDEX > 0 && head.INDEX < 32) || (head.INDEX > 992 && head.INDEX < 1024)) {
+            isGameOver = true;
+        }
+        //判断自己吃自己
+        for (let i = 0; i < body.length - 1; i++) {
+            if (body[i].INDEX == head.INDEX) isGameOver = true;
+        }
+
+        if (isGameOver){
+            alert('Game Over');
+            location.reload();
+        }
+    }
+    //清空Interval
+    this.clearAllInterval = function () {
+        let j = setInterval(() => { }, 0);
+        for (let i = j; i >= 0; i--) {
+            clearInterval(i);
+        }
+    }
+}
+
+/**
+ * @param KeyBoard 键盘类
+ */
+function KeyBoard() {
+    this.listener = function () {
+        switch (event.key.toLowerCase()) {
+            case 'w':
+                snake.walk(1);
+                break;
+            case 'd':
+                snake.walk(2);
+                break;
+            case 's':
+                snake.walk(3);
+                break;
+            case 'a':
+                snake.walk(4);
+                break;
+        }
+    }
+}
+
+/**
+ * @param Score 分数
+ */
+function Score() {
+    this.score = 0;
+    this.addScore = function(){
+        this.score++;
+        document.getElementById('score').innerText = this.score;
+    }
+}
+
+let map = new Map;
+let snake = new Snake;
+let food = new Food;
+let keyBoard = new KeyBoard;
+let util = new Util;
+let score = new Score;
+
+util.freshColor();
